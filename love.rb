725b5c9f3a8a353dc47fb2bd62459574bd191599
Tara Lloyd <tlloyd@Tara-Lloyds-MBP.home>
2015-10-31 16:15:41 -0400

    Tara is adding tweets file to commit

---
+++ love.rb
@@ -0,0 +1,24 @@
+def my_love
+    
+puts "Are you ready to play the love game?"
+
+x=0
+puts "x is #{x}"
+
+while (x >= 0)
+if (x.odd? == true)
+    puts "He loves me"
+    x = x + 1
+else
+puts "He loves me not"
+x = x + 1
+end
+
+puts "x is #{x}"
+
+end
+
+end
+
+my_love
+
---
+++ tweets.rb
@@ -0,0 +1,12 @@
+tweets = [
+"Tara is Awesome" ,
+"Noreen is obsessed with facebook - but she's still awesome",
+"Allison is the bomb diggity" ,
+"I don't use twitter!  Can you believe it"
+]
+t_counter = 0
+tweets.each do |tweet|
+    puts t_counter
+puts tweet
+t_counter = t_counter+1
+end
